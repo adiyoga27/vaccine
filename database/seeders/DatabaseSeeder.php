389e2e83ed6974b0fcf4c74b983cd165682b09f4
adiyoga <adiyoga27new@gmail.com>
2026-01-24 11:16:28 +0800

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -85,7 +85,7 @@ class DatabaseSeeder extends Seeder
             ['email' => 'admin@inovasisehat.com'],
             [
                 'name' => 'Admin Posyandu',
-                'password' => Hash::make('password'),
+                'password' => Hash::make('InovasiSehat2026'),
                 'role' => 'admin',
             ]
         );
